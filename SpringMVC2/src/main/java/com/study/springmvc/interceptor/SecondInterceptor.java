17cc95392cf04228f57047688fe02bcdd30b1f56
Tralo <13580548169@163.com>
2017-02-07 15:06:08 +0800

    拦截器配置

---
+++ SpringMVC2/src/main/java/com/study/springmvc/interceptor/SecondInterceptor.java
@@ -0,0 +1,27 @@
+package com.study.springmvc.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class SecondInterceptor implements HandlerInterceptor {
+
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		System.out.println("[SecondInterceptor]preHandle");
+		return true;
+	}
+
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		System.out.println("[SecondInterceptor]postHandle");
+	}
+
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		System.out.println("[SecondInterceptor]afterCompletion");
+	}
+
+}
